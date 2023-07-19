7b8ed4b9a5884bb70b8820278ba04a1bff783293
CMarchandon <camillemarchandon3@hotmail.fr>
2023-07-19 15:00:15 +0200

    updates comments

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -8,22 +8,26 @@ const Forecast = ({ data }) => {
   const forecastedDays = daysOfTheWeek.slice(dayInAWeek, daysOfTheWeek.length).concat(daysOfTheWeek.slice(0, dayInAWeek));
 
   return (
-    
+    // Render the forecast weather data
     <div className="grid-container">
-    {data.list.slice(0, 6).map((item, index) => (
-      <div className='weather-container-little' key={index}>
-        <div className="card-content">
-          <p className="day-little">{forecastedDays[index]}</p>
-          <p className='weather-little'>{item.weather[0].description}</p>
-          <p className='temperature-little'>{item.main.temp}°c</p>
+      {/* Map over the first 6 items in the data.list array */}
+      {data.list.slice(0, 6).map((item, index) => (
+        <div className='weather-container-little' key={index}>
+          <div className="card-content">
+            {/* Display the forecasted day */}
+            <p className="day-little">{forecastedDays[index]}</p>
+            {/* Display the weather description */}
+            <p className='weather-little'>{item.weather[0].description}</p>
+            {/* Display the temperature */}
+            <p className='temperature-little'>{item.main.temp}°c</p>
+          </div>
+          <div className="card-image">
+            {/* Display the weather icon */}
+            <img alt="weather icon" className='weather-icon' src={`src/icons/${item.weather[0].icon}.png`} />
+          </div>
         </div>
-        <div className="card-image">
-          <img alt="weather icon" className='weather-icon' src={`src/icons/${item.weather[0].icon}.png`} />
-        </div>
-      </div>
-    ))}
-  </div>
-  
+      ))}
+    </div>
   );
 };
 
